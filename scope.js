ce4aa0e7f3b75a99f4c4a4cc2db8db5f02bc16db
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-02-27 11:26:28 +0600

    Initial commit on Fri, Feb 27, 2026 11:26:28 AM

---
+++ scope.js
@@ -0,0 +1,12 @@
+let pi = 3.14
+function add(a, b) {
+  const result = (a+b)* factor + pi
+  return result
+}
+//console.log(factor, pi);
+add(5,10)
+function any() {
+  const localVar = 'I am local'
+  console.log(localVar);
+  
+}
