611be51357e759a8757a408b48ea3d4f1a613b92
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-02-27 12:32:59 +0600

    Auto-update on Fri, Feb 27, 2026 12:32:59 PM

---
+++ for-map.js
@@ -0,0 +1,32 @@
+// const numbers = [1,2,3,4,5,6]
+// numbers.map((number)=>{
+//   console.log(number + 2);
+  
+// })
+
+// const nums =[2,4,6,8,10]
+// const result = nums.map((num)=>{
+//   //console.log(num * 2);
+//   return num *2
+  
+// })
+
+// console.log(result);
+
+const nums = [1,2,3,4,5,6,7,8,9,0]
+nums.map((nums)=>{
+    console.log(nums);
+    
+})
+
+const numbers = [2,4,6,8,10]
+numbers.forEach((number)=>{
+  console.log(number);
+  
+})
+
+const heros = [3,5,,9,3,89,3,7,,67,36,,56,3]
+heros.forEach(function add(params) {
+  console.log(params);
+  
+})
---
+++ foreach.js
@@ -0,0 +1,43 @@
+// const heros = ['allu', 'tiger', 'hrithik', 'shahid', 'salman']
+// heros.forEach((hero)=>{
+//   console.log(hero);
+  
+// })
+
+// const heroens = ['allu', 'tiger', 'hrithik', 'shahid', 'salman']
+
+// heroens.forEach((heroen)=>{
+//  console.log(heroen);
+ 
+// })
+
+
+
+// const heross = ['saiful','allu', 'tiger', 'hrithik', 'shahid', 'salman', 'towfiq']
+// heross.forEach((single)=>{
+//   console.log(single);
+  
+// })
+
+
+// const herosss = ['abc', 'saiful','allu', 'tiger', 'hrithik', 'shahid', 'salman', 'towfiq']
+
+// herosss.forEach(function (params) {
+//   console.log(params);
+  
+// })
+
+const numbers = [0,1,2,3,4,5,6,7,8,9]
+numbers.forEach((number)=>{
+console.log(number);
+
+})
+
+const numbs = [0,1,2,3,4,5,6,7,8,9,4,6]
+
+const result = numbs.forEach((num)=>{
+  console.log(num);
+  
+})
+
+console.log(result);
